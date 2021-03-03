8b53b848289f29be778e066e6bdd8c426a4aa2b3
EfremkinAV <EfremkinAV99@gmail.com>
2021-03-03 22:52:32 +0700

    работает поиск элементов. ПРоблемы со влставкой в базу. Добавить обработчик ошибок

--- EDADEAL_Parse.py
+++ EDADEAL_Parse.py
@@ -2,6 +2,7 @@ import sqlite3
 import sys
 import traceback
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 def auth_start():
 
@@ -10,9 +11,9 @@ def auth_start():
     driver.maximize_window()
     driver.get(url_start_page)
     driver.implicitly_wait(5)
-    last_page = 2#int(driver.find_element_by_xpath("//div[9]/a[@class='b-button__root']").get_attribute('textContent'))
-    page_num = 2
-    print("Всего страниц найдено: " + str(last_page))
+    last_page = 1#int(driver.find_element_by_xpath("//div[9]/a[@class='b-button__root']").get_attribute('textContent'))
+    page_num = 1
+    #print("Всего страниц найдено: " + str(last_page))
 
     sqlite_connection = sqlite3.connect('edadeal_db.db')
     cursor = sqlite_connection.cursor()
@@ -26,15 +27,17 @@ def auth_start():
             gname = gcard.find_element_by_class_name("b-offer__description").text
             gprice_dis = gcard.find_element_by_class_name("b-offer__price-new").text
             gprice_old = gcard.find_element_by_class_name("b-offer__price-old").text
-            #gmarket = gcard.find_element_by_class_name("b-image b-image_disabled_false b-image_cap_f b-image_img_vert b-image_loaded_true b-offer__retailer-icon").get_attribute('title')
+            gmarket = gcard.find_element_by_class_name("b-image.b-image_disabled_false.b-image_cap_f.b-image_img_vert.b-image_loaded_true.b-offer__retailer-icon").get_attribute("title")
             page_num += 1
-            print(gname)
-            print(gprice_old)
-            print(gprice_dis)
+            #a.p-offers__offer:nth-child(1) > div.b-offer.b-offer_disabled_false.b-offer_view_default.b-offer_is-outdated_false.b-offer_is-added-to-cart_false:nth-child(1) > div.b-offer__root:nth-child(1) > div.b-offer__header:nth-child(1) > div.b-offer__offer-info:nth-child(2) > div.b-offer__dates:nth-child(2) > div.b-image.b-image_disabled_false.b-image_cap_f.b-image_img_vert.b-image_loaded_true.b-offer__retailer-icon:nth-child(1) > div.b-image__root:nth-child(1) > img
+            ##v<div title="Лента Гипермаркет" class=""><div class="b-image__root"><div class="b-image__cap b-image__cap_loading_false"
+            #print(gname)
+            #print(gprice_old)
+            #print(gprice_dis)
             #print(gmarket)
             try:
                 #ДБАВИТЬ ПРОПКУСК ОШИБОК ЕЛСИ ЭЛЕМЕНТ НЕ НАЙДЕН!!!!
-                cursor.execute('INSERT INTO EDADEAL_GOODS (name,price_old,price_dis) VALUES (?,?,?)', (gname, gprice_old, gprice_dis))
+                cursor.execute('INSERT INTO EDADEAL_GOODS (name,price_old,price_dis,market) VALUES (?,?,?,?)', (gname, gprice_old, gprice_dis, gmarket))
                 sqlite_connection.commit()
                 print("Запись успешно вставлена в таблицу edadeal_db ", cursor.rowcount)
             except sqlite3.Error as error:
@@ -44,6 +47,14 @@ def auth_start():
                 print("Печать подробноcтей исключения SQLite: ")
                 exc_type, exc_value, exc_tb = sys.exc_info()
                 print(traceback.format_exception(exc_type, exc_value, exc_tb))
+            except NoSuchElementException as err:
+                print("Не могу найти элемент")
+                print("Класс исключения: ", err.__class__)
+                print("Исключение", err.args)
+                print("Печать подробноcтей исключения: ")
+                exc_type, exc_value, exc_tb = sys.exc_info()
+                print(traceback.format_exception(exc_type, exc_value, exc_tb))
+
     cursor.close()
     sqlite_connection.close()
 # ---------------------------------------------
